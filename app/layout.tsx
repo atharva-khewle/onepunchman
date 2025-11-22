c86a7c1882d6edf00c6d928e8df27f162fc614bb
Atharva <atharvamainwork@gmail.com>
2025-11-22 09:51:10 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <main className="">
           {children}
         </main>
+        <Analytics />
       </body>
     </html>
   );
